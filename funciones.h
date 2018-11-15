fe413e3593ba245e353a93adaa7f36f7f3c19a6e
Gerardo <gerh2712@outlook.com>
2018-11-15 12:16:29 -0600

    Replanteamiento, finalizado la solicitud y verificacion de patrones

--- funciones.h
+++ funciones.h
@@ -61,29 +61,74 @@ INCERTIDUMBRE
             n = numero de lecturas
 
 */
+#define cleanScreen "cls"
 
 /*    Prototipo de funciones  */
+void bienvenida();
 void llenarArreglo(float arreglo[], int arregloSize);
-void mostrarArreglo(float arreglo[],int arregloSize);
+void mostrarArreglo(float arreglo[], int arregloSize, char variosV, char tipoValor[50], char lecturaValor[50]);
 
 /*    Funciones   */
+
+//Bienvenida
+void bienvenida(){
+    /*  Da el nombre de las personas que realizaron el programa */
+    system("cls"); 
+    printf("\n\nPrograma que determina las características dinámicas de un instrumento.");
+    printf("\n\n\tRealizado por la Brigada #4: ");
+    printf("\n\n\t\tBueno Hernández Jorge Gerardo");
+    printf("\n\n\t\tMoreno Peralta Ángel Eduardo");
+    printf("\n\n\t\tBueno Hernández Jorge Gerardo ");
+    printf("\n\n\t\tBueno Hernández Jorge Gerardo ");
+    printf("\n\n\t\tBueno Hernández Jorge Gerardo \n\n");
+    system("pause");
+    system(cleanScreen);
+}
 
 //Llena un arreglo de patrones
 void llenarArreglo(float arreglo[], int arregloSize){
       int i;
 
       for(i=0; i<arregloSize; i++){
-            printf("\n\nIntroduce el valor patron [%i]", i+1);
+            printf("\n\nIntroduce el valor patron [%i]:     ", i+1);
             scanf("%f", &arreglo[i]);
       }
 
 }
 
 //Muestra el arreglo
-void mostrarArreglo(float arreglo[], int arregloSize){
+void mostrarArreglo(float arreglo[], int arregloSize, char variosV, char tipoValor[50], char lecturaValor[50]){
       int i;
-      
-      for(i=0; i<arregloSize; i++){
-            printf("\n\nEl valor patrón [%i] es: %.2f", i+1, arreglo[i]);
+
+      if(variosV=="S"){
+            ;
+      }else if(variosV=="N"){
+                  printf("\n\n\t_________________________________________");
+                  printf("\n\n\t|   Valor %c      |     Es igual a      |");
+            for(i=0; i<arregloSize; i++){
+                  printf("\n\n\t_________________________________________");
+                  printf("\n\n\t|   Valor %c [%i]     |     %.2f");
+            }
+                  printf("\n\n\t_________________________________________");
+
       }
 }
+float promedio(float arregloLecturas[],int lecturasSize){
+      int i,cntCal=0;
+	float promedio;
+	for(i=0;i<lecturasSize;i++){
+		cntCal+=cntCal+arregloLecturas[i];
+	}
+	promedio = cntCal/lecturasSize +.0;
+	return promedio;
+}
+//Calcula el error de exactitud
+//%EE= |(Vp-Vl)/Vp| * 100
+//%EP= |(Vl-Vla)/Vl| * 100
+//%E= 100% - %EE
+//%P= 100% -$EP
+/*
+int errorExactitud(float arregloPatron[],int patronSize, float promedio(float arregloLecturas[],int lecturasSize)){
+      float errorExactitud;
+      errorExactitud=((promedio(arregloLecturas[],lecturasSize)-))
+} */
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include    <string.h>
 #include    <ctype.h>
 #include    <math.h>
+#include    <locale.h>
 #include    "funciones.h"
 
 //Variables Globales
@@ -13,127 +14,86 @@
 
 int main()
 {
-    //Con estos define se puede ampliar la capacidad máxima del arreglo 
-    #define vP 250 
-    #define vL 250
-    int numVP, numVL, i, j, k;
-    float arregloVP[vP], arregloVL[vL], arregloComodin[vP];
-    char datosCorrectos, menu;
-    
-    //Bienvenida    
-    printf("\nPrograma que determina las características dinámicas de un instrumento.");
-    printf("\n\tRealizado por la Brigada #4: ");
-    printf("\n\t\tBueno Hernández Jorge Gerardo");
-    printf("\n\t\tMoreno Peralta Ángel Eduardo");
-    printf("\n\t\tBueno Hernández Jorge Gerardo ");
-    printf("\n\t\tBueno Hernández Jorge Gerardo ");
-    printf("\n\t\tBueno Hernández Jorge Gerardo \n\n");
+    setlocale(LC_CTYPE, "Spanish");
+
+    #define cleanScreen "cls"
+    //El número de vP y vL debe de coincidir
+    #define vP 100
+    #define vL 100
+    #define lPP 100
+
+    float mVP[vP], mVL[vL], mlPP[lPP];
+    int numVP, numlPP, i; 
+    char vCorrectos;
+
+    //Bienvenida   
+    bienvenida();
+
+    //Instrucciones
+    printf("\n\nNecesitamos saber cuántos valores patrón vas a utilizar, así como si tienes el promedio de las lecturas por valor patrón, o si vas a introducir las lecturas.\n\n\tNOTA: El número de lecturas por valor patrón debe de ser el mismo para todos los valores patrón.\n\n\tNOTA: Puedes introducir un máximo de %i VALORES PATRÓN y de %i LECTURAS POR PATRÓN", vP, lPP);
+    printf("\n\n\t|   Valores Patrón (x)  |   Valores Leídos Promedio (y) |\n\n");
     system("pause");
-    system("cls");
+    system(cleanScreen);
 
-        
-    //Obtine el número de patrones y lecturas y verifica que sean mayores a cero
-    
-    printf("\n\nNecesitamos saber cuántos valores patrones y cuántos valores leídos vas a introducir, máximo %i de valores patrón y %i de valores leídos por cada valor patrón", vP, vL);
-
-    //Verifica que el valor introducido sea mayor a cero 
-    do{
-        printf("\n\nIntroduce el número de valores PATRÓN que vas a utilizar: ");
-        scanf("%i", &numVP);
-        if(numVP<=0){
-            system("cls");
-            printf("\nERROR: Introduce un valor mayor a cero");
-        }
-    } while (numVP<=0);
-
-    //Obtiene y corrobora que los valores patrón sean correctos
+    //Pide los valores patrón
     do
     {
-        //Llena el arreglo de valores patrón
-        llenarArreglo(arregloVP, numVP);
+        printf("\n\n\tIntroduce cuántos valores patrón vas a utilizar (0-50): ");
+        scanf("%i", &numVP);
+        
+        if (numVP<=0 || numVP>vP) {
+            system(cleanScreen);
+            printf("\n\nERROR:  Debes introducir un mínimo de 1 valor patrón y un máximo de %i valores patrón", vP);
+            printf("\n\nIntenta de nuevo...");
+        }
+        
+    } while (numVP<=0 || numVP>vP);
 
-        //Recorre el arreglo
-        mostrarArreglo(arregloVP, numVP);        
-
-        //Veirifica que la opción sea válida
+    
+    //Llena el arreglo de valores patrón y verifica que sean los valores deseados 
+    do
+    {
+        system(cleanScreen);
+        printf("\n\nAhora te vamos a pedir que introduzcas los valores patrón.\n\n\tIMPORTANTE: Debes de tener cuidado, ya que aceptaremos valores positivos y negativos decimales");
+        llenarArreglo(mVP, numVP);
+        system(cleanScreen);        
+        
         do
         {
-            printf("\n\n¿Los valores son correctos?");
-            printf("\n\nS)Sí\tN)No\n\n");
-            printf("\n(Introduce S o N):  "); 
+            //Muestra el arreglo
+                printf("\n\nIntroduciste los siguientes valores:    ");
+                printf("\n\n\t_________________________________________________");
+                printf("\n\n\t|   Valor Patrón          |     Es igual a      |");
+            for(i=0; i<numVP; i++){
+                printf("\n\n\t_________________________________________________");
+                printf("\n\n\t|   Valor Patrón [%i]     |     %.2f            |", i+1, mVP[i]);
+            }
+                printf("\n\n\t_________________________________________________");
+                printf("\n\n");
+            
+            //Pregunta si los valores son correctos
+            printf("\n\n't¿Los valores que instroduciste son correctos?");
+            printf("\n\nS)Sí    N)No   : ");
             while(getchar()!='\n');
-            scanf("%c", &datosCorrectos);
-            datosCorrectos = toupper(datosCorrectos);
+            scanf("%c", &vCorrectos);
+            vCorrectos = toupper(vCorrectos);
+            printf("\n\n");
 
-            if(datosCorrectos!='S' && datosCorrectos!='N'){
-                system("cls");
-                printf("\n\nIntroduce una opción válida");
+            if(vCorrectos!='S' && vCorrectos!='N'){
+                system(cleanScreen);
+                printf("\n\nEscoge una opción válida...");
             }
 
-        } while (datosCorrectos!='S' && datosCorrectos!='N');
-
-        if(datosCorrectos=='N'){
-            system("cls");
-            printf("\n\nTe volveremos a preguntar los datos");
-        }
-
-    } while (datosCorrectos!='S');
-    
-    
-    system("cls");           
-    
-
-    //Muestra un menú para que el usuario escoga qué quiere obtener
-    system("cls");
-    do
-    {           
-        printf("\n\n¿Qué quieres obtener?\n\n");
-        printf("\n");
-        printf("\n\n\ta) Promedio (Valores leídos) - Porcentaje de Error de Exactitud - Porcentaje de Exactitud");
-        printf("\n\n\tb) Promedio (Valores leídos) - Porcentaje de Error de Precisión - Porcentaje de Precisión");
-        printf("\n\n\tc) Sensibilidad del instrumento");
-        printf("\n\n\td) Error aleatorio - Incertidumbre");
-        printf("\n\n");
-        
-        while(getchar()!='\n');
-        scanf("%c", &menu);
-        menu = toupper(menu);
-        
-        switch (menu)
-        {
-            case 'A':
-                //promedio
-                //Porcentaje de Error de Exactitud 
-                //Porcentaje de Exactitud
-                break;
-
-            case 'B':
-                //promedio
-                //Porcentaje de Error de Precisión
-                //Porcentaje de Precisión
-                break;
-
-            case 'C':
-                //Sensibilidad
-                break;
-
-            case 'D':
-                //Error Aleatorio
-                //Incertidumbre
-                break;
-        
-            default:
-                system("cls");
-                printf("\n\nPor favor, introduce una opción válida");
-                break;
-        }
+        } while (vCorrectos!='S' && vCorrectos!='N');
         
 
-    } while (menu!='A' && menu!='B' && menu!='C' && menu!='D');
+    } while (vCorrectos=='N');
     
 
 
     
     return 0;
 }
+
+
 
